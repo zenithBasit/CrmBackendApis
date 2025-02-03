c5c68434c83d45fcea88337dab473dfacc10cd0d
faizZen <Faizm@zenithive.com>
2025-02-03 17:11:38 +0530

    Add files via upload

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 
 require (
 	github.com/agnivade/levenshtein v1.2.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
--- internal/graphql/handler.go
+++ internal/graphql/handler.go
@@ -14,6 +14,7 @@ import (
 	"github.com/Zenithive/it-crm-backend/auth"
 	"github.com/Zenithive/it-crm-backend/internal/graphql/generated"
 	"github.com/Zenithive/it-crm-backend/internal/graphql/schema"
+	"github.com/go-chi/cors"
 	"github.com/vektah/gqlparser/v2/ast"
 )
 
@@ -22,7 +23,19 @@ const defaultPort = "8080"
 func init() {
 	initializers.ConnectToDatabase()
 }
+
+// Must contain 6 characters, one uppercase, one lowercase, one number, and one special character
 func Handler() {
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},                      // Allow all origins
+		AllowedMethods:   []string{"GET", "POST", "OPTIONS"}, // Allow common methods
+		AllowedHeaders:   []string{"*"},                      // Allow all headers
+		ExposedHeaders:   []string{"Content-Type"},
+		AllowCredentials: false,
+	})
+
+	// handler := c.Handler(http.DefaultServeMux)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
@@ -36,8 +49,8 @@ func Handler() {
 	srv.Use(extension.AutomaticPersistedQuery{
 		Cache: lru.New[string](100),
 	})
-	http.Handle("/", playground.Handler("GraphQL playground", "/"))
-	http.Handle("/graphql", auth.Middleware(srv))
+	http.Handle("/", auth.Middleware(playground.Handler("GraphQL playground", "/")))
+	http.Handle("/graphql", c.Handler(auth.Middleware(srv)))
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
